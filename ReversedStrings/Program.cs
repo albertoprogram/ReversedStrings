2fde63ca1193bce1d990e37b7eac56230ff49a09
Alberto Chirinos <developernet@outlook.com>
2021-06-14 18:55:54 -0500

    Add project files.

---
+++ ReversedStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReversedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string / Ingrese una cadena por favor:");
+
+            string str = Console.ReadLine();
+
+            string strReversed = Solution(str);
+
+            Console.WriteLine(strReversed);
+
+            Console.WriteLine("do you wish to continue? / ¿Desea continuar?");
+            Console.WriteLine("true or false");
+
+            bool decision = Convert.ToBoolean(Console.ReadLine());
+
+            if (decision == true)
+            {
+                Main(args);
+            }
+        }
+
+        static string Solution(string str)
+        {
+            char[] strArray = str.ToCharArray();
+
+            Array.Reverse(strArray);
+
+            return new string(strArray);
+        }
+    }
+}
